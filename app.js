1935414137c26123987e264caf53e16091830b1d
Cecyl Mayoral <cecylmayoral@gmail.com>
2024-09-24 09:44:50 -0500

    Tercer Backend

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const cors = require('cors');
 const app = express();
 
 app.use(express.json());
-app.use(cors({origin:'http://localhost:4200'}));
+app.use(cors({origin:'*'}));
 app.use('/api/usuarios', require('./routes/usuario_routes'));
 app.use('/api/empleados', require('./routes/empleado_routes'));
 app.listen(3005, () => {
